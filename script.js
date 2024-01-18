353cdbcba8b388957cf3498c8144b451b9d7789e
Rahul Yadav <119389899+Yadav123Rahul@users.noreply.github.com>
2024-01-18 18:51:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
 
+function generateTodo() {
+    const newTodoInput =document.getElementById("newTodoInput").value;
+
+    const add =document.getElementById("add");
+
+    const list=document.createElement('li');
+
+    list.innerText=newTodoInput;
+    add.appendChild(list);
+}
